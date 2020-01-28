976ce84f040d56aa40a3ed63b43df727a79140f4
Christine Treacy <christine220m@gmail.com>
2020-01-27 20:46:26 -0500

    added routes to app.js

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
-const data = require('data.json');
+const data = require('./data.json');
 
+const app = express();
+
+/////MIDDLEWARE/////
 //sets view engine to use pug template engine
 app.set('view engine', 'pug');
 
-//
-app.use('static', 'public');
+//serves static files in public folder
+app.use(express.static('public'));
+
+/////ROUTES/////
+//home route
+app.render('/', [data.projects], (req, res) => {
+    res.send(`<h1>Testing, testing...</h1>`);
+});
+
+//about route
+// app.render('/about', (req, res) => {
+
+// })
+
+//dynamic project route
+
+
+
+//starts server
+app.listen(3000, () => {
+    console.log("Application is connected");
+});
